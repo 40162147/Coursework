6e74710507a33210cca2cfb45a7173534f1cbe80
NAPIER-MAIL\40162147 <sgraham975@hotmail.com>
2016-11-21 12:03:48 +0000

    grid layout done

--- Coursework Game/src/Main.java
+++ Coursework Game/src/Main.java
@@ -1,6 +1,4 @@
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 
 public class Main 
@@ -8,18 +6,8 @@ public class Main
 
 	public static void main(String[] args) 
 	{
-        SwingUtilities.invokeLater(new Runnable()
-        {
-        	public void run() 
-        	{
-    
-        		UIManager.put("swing.boldMetal", Boolean.FALSE);
-                 
-        		Grid.createAndShowGUI();
+       
 
-        	}
-
-        });
 	}
 	
 	
